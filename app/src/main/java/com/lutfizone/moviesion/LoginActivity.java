68ffee5eac3b52f9bc42786c7ca1bb684106ecaa
Lutfi Dian M <lutfidiann@gmail.com>
2020-01-03 11:36:45 +0700

    revisi login

---
+++ app/src/main/java/com/lutfizone/moviesion/LoginActivity.java
@@ -0,0 +1,81 @@
+package com.lutfizone.moviesion;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.lutfizone.moviesion.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+    private PreferenceLogin login;
+    private EditText etNim; // form input nim
+    private EditText etPass; // form input password
+    private Button btnLogin; // tombol login
+
+    @Override
+    public int setLayout() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    public void initComponent(Bundle savedInstanceState) {
+        login = PreferenceLogin.getInstance(getApplicationContext());
+        login.setPass("123456");
+    }
+
+    @Override
+    public void findView() {
+        etNim = findViewById(R.id.login_Nim);
+        etPass = findViewById(R.id.loginPass);
+        btnLogin = findViewById(R.id.btn_login);
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+
+    @Override
+    public void loadView() {
+
+    }
+
+    @Override
+    public void listener() {
+        btnLogin.setOnClickListener(v -> {
+            // ambil isi dari form
+            String nim = etNim.getText().toString();
+            String pass = etPass.getText().toString();
+            // pengecekan nama dan nim
+            if ((nim.equals("17.12.0210")
+                    || nim.equals("17.12.0216")
+                    || nim.equals("17.12.0228"))
+                    && pass.equals("123456")) {
+                // set login
+                login.setLogin(true);
+                login.setNim(nim);
+
+                Toast.makeText(this, "Login Berhasil", Toast.LENGTH_SHORT).show(); // tampilkan psesan berhasil login
+
+                // pindah ke tampilan main
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
+
+            } else if (nim.equals("") && pass.equals("")) {
+
+                Toast.makeText(this, "Nim dan Password harus diisi", Toast.LENGTH_SHORT).show();
+
+            } else {
+
+                Toast.makeText(this, "Input Nim atau Password anda salah", Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+    }
+
+}
---
+++ app/src/main/java/com/lutfizone/moviesion/base/BaseActivity.java
@@ -0,0 +1,21 @@
+package com.lutfizone.moviesion.base;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity implements BaseCompponent.Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(setLayout());
+        initComponent(savedInstanceState);
+        findView();
+        initView();
+        loadView();
+        listener();
+    }
+
+}
+
